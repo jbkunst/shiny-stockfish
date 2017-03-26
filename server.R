d98d98839788dd1900c62890566a722bf4d90db3
Joshua Kunst <jbkunst@gmail.com>
2017-03-26 05:49:29 -0300

    idk

--- server.R
+++ server.R
@@ -17,7 +17,7 @@ shinyServer(function(input, output, session) {
     process_write(handle, sprintf("position fen %s\n", chss$fen()))
     logjs(process_read(handle, PIPE_STDOUT, timeout = TIMEOUT_INFINITE))
     
-    process_write(handle, sprintf("go depth %s\n", 20))
+    process_write(handle, sprintf("go depth %s\n", ifelse(chss$turn() == "w", input$depth1, input$depth2)))
     while(TRUE) {
       message("loop")
       out <- process_read(handle, PIPE_STDOUT, timeout = 1000)
