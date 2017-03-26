47d39ba86a72f563724a6613fe0f95b54c2acc54
Joshua Kunst <jbkunst@gmail.com>
2017-03-26 05:56:36 -0300

    idk

--- ui.R
+++ ui.R
@@ -9,8 +9,8 @@ shinyUI(
         sliderInput("depth1", "Depth for white", min = 1, max = 5, value = 2),
         sliderInput("depth2", "Depth for black", min = 1, max = 5, value = 3)
         ),
-      column(4, chessboardjsOutput("board")),
-      column(4, verbatimTextOutput("console"))
+      column(4, chessboardjsOutput("board"))
+      # column(4, verbatimTextOutput("console"))
       )
     )
   )
